8df894f5d97363b266784c7a20e2472445f64370
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-05-27 19:22:27 -0400

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting:" + err.stack);
-        return;
-    }
-    // console.log("connected as id " + connect.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+
+
+}
+
+connection.connect();
 module.exports = connection;
